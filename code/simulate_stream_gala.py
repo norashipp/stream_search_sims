03bd89e1dc51cfb41d0a2ea567860c4f4dbae6e0
Nora Shipp <norashipp@gmail.com>
2023-04-06 15:50:54 -0400

    to do list

--- code/simulate_stream_gala.py
+++ code/simulate_stream_gala.py
@@ -15,13 +15,13 @@ plt.ion()
 pot = gp.BovyMWPotential2014()
 H = gp.Hamiltonian(pot)
 
+# Distribution function - determines how particles are stripped from progenitor
+df = ms.FardalStreamDF()
+
 # Progenitor coordinates
 # Example, eventually want to think about how we will sample over positions and velocities
 prog_w0 = gd.PhaseSpacePosition(pos=[10, 0, 0.] * u.kpc,
                                 vel=[0, 170, 0.] * u.km / u.s)
-
-# Distribution function - determines how particles are stripped from progenitor
-df = ms.FardalStreamDF()
 
 # Will also sample over prog masses
 prog_mass = 2.5E4 * u.Msun
@@ -40,3 +40,9 @@ stream.plot(['x', 'y'])
 # convert to ra, dec
 stream_c = stream.to_coord_frame(coord.ICRS)
 prog_c = prog.to_coord_frame(coord.ICRS)
+
+# Next steps:
+# -  Mock observations: sample isochrone, convolve with DES photometric uncertainties
+# -  Inject into DES data (or DECaLS DR10): apply isochrone selection, bin into healpix, add to maps
+# -  Decide sampling: over progenitor coordinates, masses, stripping times, isochrone parameters
+# -  Decide on stripping model (every time step?)
